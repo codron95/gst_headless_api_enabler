fe725f51044e9db60035054197eb6b6e55768a59
Rohan <rohan.sharma@ziploan.in>
2019-08-05 13:21:22 +0530

    - Some error case handlings in case of unexpected errors - enable api locks gpm object before working and releases after working - enable api returns new captcha incase there was a problem logging in.

--- web_server/controllers.py
+++ web_server/controllers.py
@@ -4,7 +4,6 @@ from selenium.common.exceptions import TimeoutException
 
 from headless_api.gst_portal_mapper import GSTPortalMapper
 from headless_api.exceptions import LoginException
-from headless_api.entities import BrowserSession
 
 from web_server.entities import JsonResponse
 
@@ -24,36 +23,48 @@ def ping(request, **kwargs):
 
 
 def captcha(request, **kwargs):
-    ps = kwargs['persistent_store']
+    drivers = kwargs['drivers']
 
-    gpm = GSTPortalMapper()
-    base64 = gpm.get_captcha_base64()
     ts = datetime.now()
 
-    ps[gpm.driver.session_id] = {
-        "session_url": gpm.driver.command_executor._url,
+    gpm = GSTPortalMapper()
+
+    drivers[gpm.driver.session_id] = {
+        "driver": gpm.driver,
+        "lock": False,
         "ts": ts
     }
+
+    try:
+        base64 = gpm.get_captcha_base64()
+    except TimeoutException:
+        return JsonResponse(
+            500,
+            "Error occured while generating token.",
+            errors=['Error occured while generating token.']
+        )
 
     response_data = {
         "token": gpm.driver.session_id,
         "captcha_base64": base64,
-        "ts": ts.strftime("%Y-%m-%d %H:%M")
+        "ts": ts.strftime("%Y-%m-%d %H-%M")
     }
 
     return JsonResponse(200, "Token generated", response_data)
 
 
 def enable_api(request, **kwargs):
+
     token = request.meta['update_entity_id']
-    ps = kwargs['persistent_store']
+    drivers = kwargs['drivers']
 
     username = request.data['username']
     password = request.data['password']
     captcha = request.data['captcha']
+
     try:
-        browser_session = BrowserSession(ps[token]['session_url'], token)
-    except KeyError as e:
+        drivers[token]['lock'] = True
+    except KeyError:
         return JsonResponse(
             404,
             "Token not found",
@@ -61,29 +72,28 @@ def enable_api(request, **kwargs):
             ["Token not found"]
         )
 
-    gpm = GSTPortalMapper(browser_session)
+    gpm = GSTPortalMapper(drivers[token]['driver'])
 
     try:
         gpm.login(username, password, captcha)
     except LoginException as e:
-        # cleanup the gpm object
-
-        captcha_base64 = gpm.get_captcha_base64()
-
-        ps[token]['captcha_base64'] = captcha_base64
-        ps[token]['ts'] = datetime.now()
 
         if e.code == -1:
+
+            gpm.cleanup()
+            del drivers[token]
+
             return JsonResponse(
                 500,
                 "Login Unsuccessful",
-                {
-                    "token": token,
-                    "captcha_base64": captcha_base64,
-                    "ts": datetime.now().strftime("%Y-%m-%d %H:%M")
-                },
+                {"token": token},
                 ["Login Unsuccessful"]
             )
+
+        ts = datetime.now()
+        captcha_base64 = gpm.get_captcha_base64()
+        drivers[token]['ts'] = ts
+        drivers[token]['lock'] = False
 
         return JsonResponse(
             400,
@@ -91,7 +101,7 @@ def enable_api(request, **kwargs):
             {
                 "token": token,
                 "captcha_base64": captcha_base64,
-                "ts": datetime.now().strftime("%Y-%m-%d %H:%M")
+                "ts": ts.strftime("%Y-%m-%d %H:%M")
             },
             [str(e)]
         )
@@ -99,11 +109,8 @@ def enable_api(request, **kwargs):
     try:
         gpm.enable_api_access()
     except TimeoutException as e:
-        # cleanup the gpm object
         gpm.cleanup()
-
-        # Delete reference to it from our persistent_store to clear memory
-        del ps[token]
+        del drivers[token]
 
         return JsonResponse(
             500,
@@ -112,11 +119,8 @@ def enable_api(request, **kwargs):
             ["Error. Timed Out."]
         )
 
-    # cleanup the gpm object
     gpm.cleanup()
-
-    # Delete reference to it from our persistent_store to clear memory
-    del ps[token]
+    del drivers[token]
 
     return JsonResponse(
         200,
